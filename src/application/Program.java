c63d9efcc4e67e896a647553964b1e57aa402e99
Julio Donofreo <julio.bibico@hotmail.com>
2023-04-13 17:14:18 -0300

    Leitura de dados implementada

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) throws URISyntaxException {
 
+        Scanner sc = new Scanner(System.in);
         ClassLoader classLoader = Program.class.getClassLoader();
         Path path = Path.of(classLoader.getResource("application/Program.class")
                 .toURI())
@@ -17,7 +22,16 @@ public class Program {
                 .getParent()
                 .getParent()
                 .resolve("src/file/in.csv");
-        System.out.println(path);
+
+        System.out.print("Enter salary: ");
+        double salary = sc.nextDouble();
+
+        try (BufferedReader bf = new BufferedReader(new FileReader(path.toFile()))){
+
+            List<String> lines = bf.lines().toList();
+        } catch (IOException e) {
+            System.out.println("ERROR! " + e.getMessage());;
+        }
 
     }
 }
